fd38c0399e88a33b59c8f3ba9e5c71333804f717
CaptainSP <19982002ke@gmail.com>
2021-12-24 20:45:12 +0300

    deploy

--- index.js
+++ index.js
@@ -70,6 +70,6 @@ app.post("/notice", (req, res) => {
     res.send("Success");
 });
 
-app.listen(1224, () => {
-    console.log('HTTP server running on port 1224');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('HTTP server running on port 80');
 });
